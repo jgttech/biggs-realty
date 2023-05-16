68be6a93fce2c58702df61ea5b1b4a0439524d0d
Jonathon Tech <65690496+jgttech@users.noreply.github.com>
2023-05-16 15:10:52 -0500

    Release 1.0.2

--- apps/chatbot-embed/src/index.tsx
+++ apps/chatbot-embed/src/index.tsx
@@ -3,24 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'views/App';
 
-window.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => {
-    const MOUNT_ID = 'biggs-realty-chatbot-embed';
-    const hasMount = document.getElementById(MOUNT_ID);
-    const body = document.getElementsByTagName('body')[0];
-    const root = document.createElement('div');
+const MOUNT_ID = 'biggs-realty-chatbot-embed';
+const hasMount = document.getElementById(MOUNT_ID);
+const body = document.getElementsByTagName('body')[0];
+const root = document.createElement('div');
 
-    // Add the mount ID.
-    root.setAttribute('id', MOUNT_ID);
+// Add the mount ID.
+root.setAttribute('id', MOUNT_ID);
 
-    if (!hasMount) {
-      body.appendChild(root);
-    }
+if (!hasMount) {
+  body.appendChild(root);
+}
 
-    ReactDOM.createRoot(root).render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-  }, 0);
-});
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
