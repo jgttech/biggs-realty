28e0fbe82fb484b48f28cab713efebd5faa95e92
Jonathon Tech <65690496+jgttech@users.noreply.github.com>
2023-05-16 23:13:43 -0500

    Release 1.1.2

--- apps/chatbot-embed/dist/index.js
+++ apps/chatbot-embed/dist/index.js
@@ -2,15 +2,15 @@
     document.addEventListener('DOMContentLoaded', () => {
       const body = document.getElementsByTagName('body')[0];
       const head = document.getElementsByTagName('head')[0];
-      const RAW_URL = `https://rawcdn.githack.com/jgttech/biggs-realty/1.1.1`;
+      const RAW_URL = `https://rawcdn.githack.com/jgttech/biggs-realty/1.1.2`;
       
       const css_0 = document.createElement('link');
-      css_0.setAttribute('href', `${RAW_URL}/apps/chatbot-embed/dist/assets/index-c874adbb.css`);
+      css_0.setAttribute('href', `${RAW_URL}/apps/chatbot-embed/dist/assets/index-bce03506.css`);
       css_0.setAttribute('rel', 'stylesheet');
     
       
       const js_0 = document.createElement('script');
-      js_0.setAttribute('src', `${RAW_URL}/apps/chatbot-embed/dist/assets/index-c7174d8c.js`);
+      js_0.setAttribute('src', `${RAW_URL}/apps/chatbot-embed/dist/assets/index-89858c75.js`);
       js_0.setAttribute('type', 'module');
     
       head.appendChild(css_0);
--- apps/chatbot-embed/src/views/Chatbox/Popover/Popover.tsx
+++ apps/chatbot-embed/src/views/Chatbox/Popover/Popover.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
+import { Portal } from 'react-portal';
 import { motion, AnimatePresence } from 'framer-motion';
+import { Cross2Icon } from '@radix-ui/react-icons';
 import { Popover } from 'components/Popover';
 import { Logo } from 'components/Logo';
 import { Box } from 'components/Box';
@@ -31,11 +33,30 @@ const styles = {
     br-text-white
     br-pr-4
   `,
+  icon: $`
+    br-absolute
+    br-top-[-2.5rem]
+    br-right-0
+    br-p-2
+    br-w-8
+    br-h-8
+    br-transition-all
+    br-duration-300
+    hover:br-w-9
+    hover:br-h-9
+    hover:br-bg-blue-500
+    br-cursor-pointer
+    br-rounded-full
+    br-bg-br-logo-blue
+    br-text-white
+  `,
 };
 
 export const PopoverChat: FC = () => {
+  const [open, setOpen] = useState(false);
   const [visible, setVisible] = useState(false);
   const handleOpen = () => {
+    setOpen(true);
     setTimeout(() => {
       const body = document.getElementsByTagName('body')[0];
       const content = document.querySelector('#biggs-realty-chatbox-content');
@@ -49,48 +70,53 @@ export const PopoverChat: FC = () => {
       }
     }, 1);
   };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     setTimeout(() => {
       setVisible(true);
-    }, 29000);
+    }, 0);
   }, []);
 
-  console.log('>>> visible:', visible);
-
   return (
-    <AnimatePresence mode='wait'>
-      {visible && (
-        <motion.div
-          initial={{
-            opacity: 0,
-          }}
-          animate={{
-            opacity: 1,
-            transition: {
-              duration: 1,
-            },
-          }}
-        >
-          <Popover>
-            <Popover.Trigger className={styles.trigger} asChild>
-              <button onClick={handleOpen} className={styles.button}>
-                <Logo className={styles.img} />
-                Client Support
-              </button>
-            </Popover.Trigger>
-            <Popover.Content
-              className='br-min-w-[400px] br-max-w-[500px] br-rounded-lg br-p-1'
-              id='biggs-realty-chatbox'
-              side='top'
-              align='end'
-              sideOffset={14}
-            >
-              <Box id='biggs-realty-chatbox-content' />
-            </Popover.Content>
-          </Popover>
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <Portal>
+      <AnimatePresence mode='wait'>
+        {visible && (
+          <motion.div
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+              transition: {
+                duration: 1,
+              },
+            }}
+          >
+            <Popover open={open}>
+              <Popover.Trigger className={styles.trigger} asChild>
+                <button onClick={handleOpen} className={styles.button}>
+                  <Logo className={styles.img} />
+                  Client Support
+                </button>
+              </Popover.Trigger>
+              <Popover.Content
+                className='br-min-w-[400px] br-max-w-[500px] br-rounded-lg br-p-1 br-bg-white'
+                id='biggs-realty-chatbox'
+                side='top'
+                align='end'
+                sideOffset={14}
+                onInteractOutside={handleClose}
+              >
+                <Box id='biggs-realty-chatbox-content' />
+                <Cross2Icon className={styles.icon} onClick={handleClose} />
+              </Popover.Content>
+            </Popover>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </Portal>
   );
 };
