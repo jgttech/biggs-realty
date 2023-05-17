ca7eb3c73e6eb4784b44af64c323e28807399aa1
Jonathon Tech <65690496+jgttech@users.noreply.github.com>
2023-05-17 13:50:05 -0500

    Release 1.1.5

--- apps/chatbot-embed/dist/index.js
+++ apps/chatbot-embed/dist/index.js
@@ -2,7 +2,7 @@
     document.addEventListener('DOMContentLoaded', () => {
       const body = document.getElementsByTagName('body')[0];
       const head = document.getElementsByTagName('head')[0];
-      const RAW_URL = `https://rawcdn.githack.com/jgttech/biggs-realty/1.1.4`;
+      const RAW_URL = `https://rawcdn.githack.com/jgttech/biggs-realty/1.1.5`;
       
       const css_0 = document.createElement('link');
       css_0.setAttribute('href', `${RAW_URL}/apps/chatbot-embed/dist/assets/index-87d1373b.css`);
@@ -10,7 +10,7 @@
     
       
       const js_0 = document.createElement('script');
-      js_0.setAttribute('src', `${RAW_URL}/apps/chatbot-embed/dist/assets/index-c7eeaf1b.js`);
+      js_0.setAttribute('src', `${RAW_URL}/apps/chatbot-embed/dist/assets/index-7606aa5a.js`);
       js_0.setAttribute('type', 'module');
     
       head.appendChild(css_0);
